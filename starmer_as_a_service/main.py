80c74715cc589415337b4a750d2fe5e2d9a39d84
alex dean <alex.dean@aac-clydespace.com>
2021-11-07 19:28:37 +0000

    just vibes

--- starmer_as_a_service/main.py
+++ starmer_as_a_service/main.py
@@ -5,56 +5,78 @@ from flask_restful import Resource, Api, abort, reqparse
 from enum import Enum
 import csv
 from pathlib import Path
+from collections import namedtuple
+Politician = namedtuple('Politician', ['picture_name', 'emotion', 'emotion_strength'])
 
 
 
-app = Flask(__name__, static_folder='../keiths')
+app = Flask(__name__, static_folder='../static')
 api = Api(app)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 
-keiths = []
+politicians = []
 
-with open('../keiths/keiths.csv') as csv_file:
+# TODO move to a sqlite db or something more sensible. 
+with open('../static/db.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
         if line_count == 0:
             print(f'Column names are {", ".join(row)}')
-            line_count += 1
         else:
-            if float(row[2]) > 0.4:
-                keiths.append((row[0], row[1], row[2]))
-            line_count += 1
+            if float(row[3]) > 0.4:
+                politicians.append(
+                    Politician(row[0], row[1], row[2], row[3])
+                )
+        line_count += 1
 
 
-keiths_by_emotion = defaultdict(list)
+politicians_by_emotion = defaultdict(dict)
 
-for keith in keiths:
-    keiths_by_emotion[keith[1]].append(keith[0])
+for politician in politicians:
+    # Need the emotion dictionary to have a list. We choose a random 
+    # picture from this list for emotion by politician. 
+    if politician.emotion not in politicians_by_emotion[politician.name]:
+        politicians_by_emotion[politician.name][politician.emotion] = []
+    politicians_by_emotion[politician.name][politician.emotion].append(
+        politician.picture_name
+    )
 
-class KeithPics(Resource):
-    def get(self, keith_file):
-        return send_from_directory(app.static_folder, keith_file, max_age=0)
+class PoliticianPics(Resource):
+    def get(self, picture_file):
+        return send_from_directory(app.static_folder, picture_file, max_age=0)
 
 
-class Keith(Resource):
+class Politicians(Resource):
     def get(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('emotion', type=str, required=True, help='Emotion that you want your Keith to have')
+        parser.add_argument(
+            'politician',
+            type=str,
+            required=True,
+            help="Which politician to choose"
+        )
+
+        parser.add_argument(
+            'emotion', 
+            type=str, 
+            required=True, 
+            help='Emotion that you want your politician to have'
+        )
         args = parser.parse_args()
 
-        keiths = keiths_by_emotion[args['emotion']]
+        politician_emotion_pictures = politicians_by_emotion[args['politician']][args['emotion']]
 
-        if keiths:
-            keith = random.choice(keiths_by_emotion[args['emotion']])
-            return redirect(api.url_for(KeithPics, keith_file=keith))
+        if politician_emotion_pictures:
+            picture_file = random.choice(politician_emotion_pictures)
+            return redirect(api.url_for(PoliticianPics, picture_file=picture_file))
         else:
-            abort(404, message=f"No Keiths found with emotion {args['emotion']}")
+            abort(404, message=f"No {args['politician']} found with emotion {args['emotion']}")
 
 
-api.add_resource(Keith, '/keith/')
-api.add_resource(KeithPics, '/keithpics/<keith_file>/')
+api.add_resource(Politicians, '/politicians/')
+api.add_resource(PoliticianPics, '/politicianpics/<picture_file>/')
 
 if __name__ == '__main__':
     app.run()
