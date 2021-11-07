a937c2c778ef002c3386970de4403b785f586814
alex dean <alex.dean@aac-clydespace.com>
2021-11-07 19:40:43 +0000

    stubbing utilities

--- starmer_as_a_service/utilities.py
+++ starmer_as_a_service/utilities.py
@@ -1,1 +1,21 @@
+from fer import FER
+import cv2
+from os import listdir, sep
+from os.path import isfile, join
+from main import Politician
 # Recreate the csv file as appropriate. 
+DIRS = ["nicola", "keith"]
+
+def get_files():
+    files_by_politicians = {}
+    for dir in DIRS:
+        static_path = ".." + sep + "static" + sep + dir
+        files_by_politicians[dir] = [f for f in listdir(dir) if isfile(join(dir, f))]
+
+def evaluate_emotions():
+    politicians = []
+
+
+if __name__ == "__main__":
+    files = get_files()
+    
